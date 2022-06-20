53e7510a3e84da01e4443f9b34a28b2dd1933d86
domino659 <martin@sionfamily.com>
2022-06-20 12:09:42 +0200

    img-foler

--- utils/captcha/captcha_text.py
+++ utils/captcha/captcha_text.py
@@ -1,18 +1,28 @@
+import os
 from time import sleep
 from selenium.common.exceptions import NoSuchElementException
+
 
 def detect_capcha(driver):
     try:
         captcha = driver.find_element_by_id("captchaForm")
-        if captcha: {
-            capcha(driver)
-        }
+        if captcha:
+            {
+                capcha(driver)
+            }
     except NoSuchElementException:
         pass
 
+
 def capcha(driver):
-    # Take screen of the captcha and save it.
-    driver.find_element_by_id("captchaImg").screenshot('img/captcha_screenshot.png')
+    # Take screen of the aptcha and save it.
+    path = 'img'
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+    driver.find_element_by_id("captchaImg").screenshot(
+        'img/captcha_screenshot.png')
     # Solve the captchat using ML
     captcha_text = resolve()
     captcha = driver.find_element_by_id("captchaChars")
@@ -20,10 +30,11 @@ def capcha(driver):
     # If fail and captcha not solved will try again
     detect_capcha(driver)
 
+
 def resolve():
     import pytesseract
 
     pytesseract.pytesseract.tesseract_cmd = r'C:\Tesseract-OCR\tesseract.exe'
     result = pytesseract.image_to_string('img/captcha_screenshot.png')
-    
+
     return result
